703157ec87c43898204102993eae24befa6595d2
squirrelsquirrel78 <15334952+squirrelsquirrel78@users.noreply.github.com>
2026-04-03 21:57:08 +0200

    2026 brand restyle: fonts, colors, and Flourish vizs

--- gcct-dashboard-main/public/index.js
+++ gcct-dashboard-main/public/index.js
@@ -33,7 +33,7 @@ async function getData() {
                 config.datasets[id] = [];
             })
             if (config.dashboard.tickers) {
-                dataURLS.push('https://public.flourish.studio/visualisation/16565310/visualisation.json') // this assumes we want the same template for all tickers
+                dataURLS.push('https://public.flourish.studio/visualisation/28342067/visualisation.json') // this assumes we want the same template for all tickers
                 dataURLS.push(`./assets/data/${config.dashboard.ticker_data}.json`)
                 config.datasets.ticker = {};
             }
